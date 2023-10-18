ff69f5cdd5dffac76e1400569380d089e2fbe626
salehnavab <salehnavabm@gmail.com>
2023-10-19 00:26:44 +0330

    Add project files.

---
+++ determinan/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] matrix = new int[3, 3];
+
+        Console.WriteLine("Enter the elements of the matrix:");
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("Entered matrix is:");
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
